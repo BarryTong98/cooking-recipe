e8518b9930242c51db0b1aa714188979a6920531
Barry <barry@Barrys-MacBook-Pro.local>
2023-01-16 23:41:45 +0800

    Adding Multiple Ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,14 +1,29 @@
 import './Create.css'
 
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 function Create() {
     const [title, setTitle] = useState('');
     const [method, setMethod] = useState('');
     const [cookingTime, setCookingTime] = useState('');
-    
+    const [newIngredient, setNewIngredient] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const ingredientInput = useRef(null)
+
     const handleSubmit = (e) => {
         e.preventDefault()
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault()
+        //take off any white space in the string
+        const ing = newIngredient.trim()
+
+        if (ing && !ingredients.includes(ing)) {
+            setIngredients(preIngredients => [...preIngredients, ing])
+        }
+        setNewIngredient('')
+        ingredientInput.current.focus()
     }
 
     return (
@@ -22,10 +37,33 @@ function Create() {
                         onChange={(e) => {
                             setTitle(e.target.value)
                         }}
-                    value={title}
-                    required
+                        value={title}
+                        required
                     />
                 </label>
+                <label>
+                    <span>Recipe ingredients:</span>
+                    <div className={"ingredients"}>
+                        <input
+                            type={"text"}
+                            onChange={(e) => setNewIngredient(e.target.value)}
+                            value={newIngredient}
+                            ref={ingredientInput}
+                        />
+                        <button
+                            className={"btn"}
+                            onClick={handleAdd}
+                        >add
+                        </button>
+                    </div>
+                </label>
+                <p>
+                    Current ingredients:
+                    {ingredients.map((i) => (
+                        <em key={i}>{i}</em>
+                    ))}
+                </p>
+
                 <label>
                     <span>Recipe method:</span>
                     <textarea
